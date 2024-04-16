ce09ffacaf30d8f33f362cb2191dd079c5b1d907
DLB <N/A>
2024-04-16 15:53:11 +0100

    shader class & loading

--- src/shader.cpp
+++ src/shader.cpp
@@ -1,3 +1,9 @@
 #include "shader.hpp"
   /* create shader program */
-const unsigned int mvp_location = glGetUniformLocation(shader_program, "u_mvp");
+// const unsigned int mvp_location = glGetUniformLocation(shader_program, "u_mvp");
+void shader::bind() {
+    glUseProgram(shader_program);
+}
+void shader::unbind() {
+    glUseProgram(GL_NONE);
+}
