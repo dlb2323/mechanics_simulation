32a8ab0b962239801ebf55792b2dbcd46d15f5d2
marlo <126981859+lemonmash@users.noreply.github.com>
2024-05-03 20:37:07 +0100

    final commit:WORKING

--- src/shader.cpp
+++ src/shader.cpp
@@ -1,14 +1,17 @@
 #include "shader.hpp"
-  /* create shader program */
-// const unsigned int mvp_location = glGetUniformLocation(shader_program, "u_mvp");
+
+// define static variables
 shader* shader::main;
 shader* shader::single_colour;
+// bind for drawing 
 void shader::bind() {
     glUseProgram(m_program);
 }
+// unbind from drawing 
 void shader::unbind() {
     glUseProgram(GL_NONE);
 }
+// return uniform location from given name 
 unsigned int shader::get_uniform_location(std::string name) {
     return glGetUniformLocation(m_program, name.c_str());
 }
--- src/shader.hpp
+++ src/shader.hpp
@@ -7,11 +7,14 @@
 #include <fstream>
 #include <iostream>
 
+// local function too copy file data into string
 static std::string read_file(std::string path) {
-  std::string data = "test";
+  std::string data;
   std::ifstream file;
+  // open file in binary mode for windows compatability
   file.open(path, std::ios_base::out | std::ios_base::binary);
   if (file.is_open()) {
+    // read in characters
     data.assign((std::istreambuf_iterator<char>(file)),
                 (std::istreambuf_iterator<char>()));
   }
@@ -19,10 +22,14 @@ static std::string read_file(std::string path) {
   return data;
 }
 
+// local function to compile a shader
 static const unsigned int load_shader(std::string source_path,
                                const unsigned int shader_object) {
+  // read in file to string
   std::string shader_source = read_file(source_path);
+  // convert to const char * format to interact with opengl
   const char *p_shader_source = shader_source.c_str();
+  // opengl set shader source and compile
   glShaderSource(shader_object, 1, (const GLchar *const *)&p_shader_source, NULL);
   glCompileShader(shader_object);
 
@@ -38,17 +45,20 @@ static const unsigned int load_shader(std::string source_path,
 }
 
 class shader {
+  // program pointer
   const unsigned int m_program;
+  // store shader uniform locations
   unsigned int m_u_mvp_location;
   unsigned int m_u_colour_location;
   unsigned int m_u_time_location;
 
 public:
+  // public access shaders
   static shader* main;
   static shader* single_colour;
   shader(std::string vertex_path, std::string fragment_path) : m_program(glCreateProgram()) {
-    /* create and link vertex and fragment shaders */
 
+    /* create and link vertex and fragment shaders */
     const unsigned int vertex_shader = load_shader(vertex_path, glCreateShader(GL_VERTEX_SHADER));
     const unsigned int fragment_shader = load_shader(fragment_path, glCreateShader(GL_FRAGMENT_SHADER));
 
@@ -71,12 +81,14 @@ public:
     m_u_time_location = get_uniform_location("u_time");
   }
   ~shader() {
+    // clean up program data
     glDeleteProgram(m_program);
   }
 
   void bind();
   void unbind();
   unsigned int get_uniform_location(std::string name);
+  // uniform getters
   unsigned int mvp_location() const { return m_u_mvp_location; }
   unsigned int colour_location() const { return m_u_colour_location; }
   unsigned int time_location() const { return m_u_time_location; }
