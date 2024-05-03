31072163329913badbccd7a489ec9a56a863ecb5
marlo <126981859+lemonmash@users.noreply.github.com>
2024-05-03 20:37:00 +0100

    camera, menu, environment and drawing:WORKING

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
+#include "glm/ext/matrix_transform.hpp"
+#include <algorithm>
 #include <iostream>
+#include <chrono>
 #define GLFW_INCLUDE_NONE
+#define DRAW_WIREFRAME
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
@@ -104,13 +108,129 @@ void APIENTRY glDebugOutput(GLenum source, GLenum type, unsigned int id,
   std::cin.get();
 }
 
-class environment {
-    glm::mat4 proj;  
-  environment(int width, int height) : proj(glm::perspective((float)(M_PI / 4), (float)width / height,
-                                      0.1f, 100.0f)) {
+glm::vec3 lerp(glm::vec3 x, glm::vec3 y, float t) {
+  return x * (1.f - t) + y * t;
 }
-  void draw();
+
+class timestamp {
+  std::chrono::time_point<std::chrono::system_clock> start;
+public:
+  timestamp() {
+  }
+  void begin() {
+    start = std::chrono::system_clock::now();
+  }
+  double get_elapsed_time() {
+    return ((std::chrono::duration<double>)(std::chrono::system_clock::now()-start)).count();
+  }
 };
+
+class camera {
+  enum MODE {
+    STILL,
+    FOCUS,
+    TRACK
+  };
+  glm::vec3 m_position;
+  glm::vec3 m_start;
+  glm::vec3 m_focus_point;
+  glm::vec3* m_p_target;
+  timestamp m_timestamp;
+  const double m_total_time = 1.0;
+  camera::MODE m_mode;
+  double smooth(double x);
+public:
+  float zoom = 1.0f;
+  float rotation = M_PI/4;
+  camera() {
+  }
+  void focus(const glm::vec3 & point);
+  void track(glm::vec3 * const target);
+
+  void update();
+
+  glm::mat4 get_view() const {
+    glm::mat4 view(1.0f);
+    glm::vec3 position = m_position + glm::vec3(0, 5, 10)*zoom; 
+    view = glm::lookAt(position, m_position, glm::vec3(0.0f, 1.0f, 0.0f));
+    return view;
+  }
+};
+
+void camera::focus(const glm::vec3 & point) {
+  m_timestamp.begin();
+  m_focus_point = point;
+  m_start = m_position;
+  m_mode = camera::MODE::FOCUS;
+}
+
+void camera::track(glm::vec3 * const target) {
+  m_timestamp.begin();
+  m_p_target = target; 
+  m_start = m_position;
+  m_mode = camera::MODE::TRACK;
+}
+
+double camera::smooth(double x) {
+  return tanh(sqrt(x)*6-M_PI)/2+0.503;
+}
+
+void camera::update() {
+  switch (m_mode) {
+    case camera::MODE::FOCUS: {
+      if (m_timestamp.get_elapsed_time() > m_total_time) {
+        m_position = m_focus_point;
+        m_mode = camera::MODE::STILL;
+      }
+      m_position = lerp(m_start, m_focus_point, smooth(m_timestamp.get_elapsed_time()/m_total_time));
+      break;
+    }
+    case camera::MODE::TRACK: {
+      if (m_p_target == NULL) {
+        m_mode = camera::MODE::STILL;
+      } else {
+        if (m_timestamp.get_elapsed_time() > m_total_time) {
+          m_position = *m_p_target;
+        } else {
+          m_position = lerp(m_start, *m_p_target, smooth(m_timestamp.get_elapsed_time()/m_total_time));
+        }
+      }
+      break;
+    }
+    case camera::MODE::STILL:
+    default:
+      break;
+  }
+}
+
+// world rendering
+class environment {
+    static glm::mat4 proj; 
+public:
+    static std::vector<sphere> spheres;
+    static camera current_camera;
+    static void init(GLFWwindow* window);
+    static void deinit();
+    static void update();
+    static void draw();
+    static sphere* create(unsigned int radius);
+};
+camera environment::current_camera;
+std::vector<sphere> environment::spheres;
+
+
+void environment::init(GLFWwindow* window) {
+}
+void environment::deinit() {
+}
+
+void environment::update() {
+}
+
+sphere* environment::create(unsigned int radius) {
+  spheres.push_back(sphere(radius));
+  return &spheres[spheres.size()-1];
+}
 
 void environment::draw() {
 }
@@ -121,10 +241,10 @@ public:
   GUI() {}
   static void show();
   static void help();
+  static void particle_options(std::string name); 
 };
 
 void GUI::show() {
-    help_count = 0;
     // Demonstrate the various window flags. Typically you would just use the default!
     static bool no_titlebar = false;
     static bool no_scrollbar = false;
@@ -163,29 +283,48 @@ void GUI::show() {
         return;
     }
 
-    ImGui::Text("dear imgui says hello! (%s) (%d)", IMGUI_VERSION, IMGUI_VERSION_NUM);
+    // camera
+    ImGui::SliderFloat("slider float (log)", &environment::current_camera.zoom, 0.0f, 10.0f, "%.4f");
+
+    // particles
+    const unsigned int input_text_charlen = 128;
+    const unsigned int particles_array_len = 50;
+    static std::string particles[particles_array_len]; 
+    ImGui::Text("list of particles: size %d", 1);
     ImGui::Spacing();
     {
-        static int clicked = 0;
-        if (ImGui::Button("Button"))
-            clicked++;
-        if (clicked & 1)
-        {
-            ImGui::SameLine();
-            ImGui::Text("Thanks for clicking me!");
+        static int code = 65;
+        char input[input_text_charlen] = {(char)code};
+        ImGui::InputText("particle name", input, IM_ARRAYSIZE(input));
+        if (ImGui::Button("add particle")) {
+          std::string input_string(input);
+          int  location = -1;
+          bool match = false;
+          for (int i = 0; i < particles_array_len; i++) {
+            if (particles[i].length() == 0 && input_string.length() > 0)
+              location = i;
+            if (particles[i] == input_string) {
+              match = true;
+              break;
+            }
+          }
+          if (location >= 0 && !match) {
+            particles[location] = input;
+            sphere* p_sphere = environment::create(5);
+            p_sphere->position = glm::vec3(std::rand()%100-50, std::rand()%100-50, std::rand()%100-50);
+            environment::current_camera.focus(p_sphere->position);
+            code++;
+          }
         }
     }
     ImGui::Spacing();
-    {
-        static int clicked = 0;
-        if (ImGui::Button("Button"))
-            clicked++;
-        if (clicked & 1)
-        {
-            ImGui::SameLine();
-            ImGui::Text("Thanks for clicking me!");
-        }
-    }
+
+    auto display = [](std::string& name) { GUI::particle_options(name); };
+    for (int i = 0; i < particles_array_len; i++) {
+      if (particles[i].length() > 0)
+        GUI::particle_options(particles[i]);
+  }
+
     ImGui::Spacing();
     
     GUI::help();
@@ -209,9 +348,28 @@ void GUI::help() {
         ImGui::BulletText("Read the FAQ at https://www.dearimgui.com/faq/");
         ImGui::BulletText("Set 'io.ConfigFlags |= NavEnableKeyboard' for keyboard controls.");
         ImGui::BulletText("Set 'io.ConfigFlags |= NavEnableGamepad' for gamepad controls.");
+    }
+}
 
-        ImGui::SeparatorText("USER GUIDE:");
-        ImGui::ShowUserGuide();
+void GUI::particle_options(std::string name) {
+    ImGui::SetNextItemOpen(true, ImGuiCond_Once);
+    if (ImGui::CollapsingHeader(name.c_str()))
+    {
+        ImGui::SeparatorText("TEST");
+        ImGui::BulletText("Sections below are demonstrating many aspects of the library.");
+        // ImGui::SeparatorText("ABOUT THIS DEMO:");
+        // ImGui::BulletText("Sections below are demonstrating many aspects of the library.");
+        // ImGui::BulletText("The \"Examples\" menu above leads to more demo contents.");
+        // ImGui::BulletText("The \"Tools\" menu above gives access to: About Box, Style Editor,\n"
+        //                   "and Metrics/Debugger (general purpose Dear ImGui debugging tool).");
+        //
+        // ImGui::SeparatorText("PROGRAMMER GUIDE:");
+        // ImGui::BulletText("See the ShowDemoWindow() code in imgui_demo.cpp. <- you are here!");
+        // ImGui::BulletText("See comments in imgui.cpp.");
+        // ImGui::BulletText("See example applications in the examples/ folder.");
+        // ImGui::BulletText("Read the FAQ at https://www.dearimgui.com/faq/");
+        // ImGui::BulletText("Set 'io.ConfigFlags |= NavEnableKeyboard' for keyboard controls.");
+        // ImGui::BulletText("Set 'io.ConfigFlags |= NavEnableGamepad' for gamepad controls.");
     }
 }
 
@@ -265,15 +423,15 @@ int main() {
   ImGui_ImplGlfw_InitForOpenGL(mainWindow.getWindow(), true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
   ImGui_ImplOpenGL3_Init();
   // end
-
+  environment::init(mainWindow.getWindow());
   int nodes = 120;
   shader sphere_shader("vertex_shader.glsl", "fragment_shader.glsl");
   const unsigned int mvp_location = sphere_shader.get_uniform_location("u_mvp");
-  const unsigned int time_location =
-      sphere_shader.get_uniform_location("u_time");
-  vertexObject sphere_object(&sphere_shader);
-  sphere::gen_vertex_data(nodes, sphere_object);
-  sphere test(&sphere_object, 5);
+  const unsigned int time_location = sphere_shader.get_uniform_location("u_time");
+  mesh sphere_mesh = mesh(&sphere_shader);
+  sphere::gen_vertex_data(nodes, sphere_mesh);
+  environment::spheres.push_back(sphere(5));
+
 
   while (!mainWindow.shouldClose()) {
     if (glfwGetKey(mainWindow.getWindow(), GLFW_KEY_ESCAPE) == GLFW_PRESS) {
@@ -283,37 +441,34 @@ int main() {
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
-    ImGui::ShowDemoWindow(); // Show demo window! :)
+    // ImGui::ShowDemoWindow(); // Show demo window! :)
     GUI::show();
     
     glClearColor(0.2, 0.3, 0.3, 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    environment::current_camera.update();
     int width, height;
     glfwGetFramebufferSize(mainWindow.getWindow(), &width, &height);
-
-    // draw
     glm::mat4 proj = glm::perspective((float)(M_PI / 4), (float)width / height,
-                                      0.1f, 100.0f);
-    glm::mat4 view = glm::mat4(1.0f);
-    view = glm::translate(view, glm::vec3(-20.0f, -20.0f, -100.0f));
-    sphere_object.bind();
-    for (int i = 0; i < 30; i++) {
-      for (int j = 0; j < 30; j++) {
+                                      0.1f, 300.0f);
+    // draw
+    glm::mat4 view = environment::current_camera.get_view();
+    // glm::mat4 view = glm::mat4(1.0);
+    sphere_mesh.bind();
+    for (int i = 0; i < environment::spheres.size(); i++) {
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(i*5*2, j*5*2, 0.0f));
+        model = glm::translate(model, environment::spheres[i].position);
         model = glm::rotate(model, (float)glfwGetTime() / 20,
                             glm::vec3(1.0f, 0.0f, 1.0f));
         model = glm::scale(
-            model, glm::vec3(test.m_radius, test.m_radius, test.m_radius));
+            model, glm::vec3(environment::spheres[i].m_radius, environment::spheres[i].m_radius, environment::spheres[i].m_radius));
         glm::mat4 mvp = proj * view * model;
         glUniformMatrix4fv(mvp_location, 1, GL_FALSE, glm::value_ptr(mvp));
         glUniform1f(time_location, (float)glfwGetTime());
-        sphere_object.draw();
-      }
-    }
-    sphere_object.unbind();
-
+        sphere_mesh.draw();
+  }
+  sphere_mesh.unbind();
 
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
