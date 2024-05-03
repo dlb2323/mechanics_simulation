a5a86afb1fa4660e4bc8b0a3468d206f27207ac4
marlo <126981859+lemonmash@users.noreply.github.com>
2024-05-03 20:36:26 +0100

    simple window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include "GLFW/glfw3.h"
 
 int main() {
-	cout << "Hello World" << std::endl;
-	return 0;
+	GLFWwindow *window;
+
+	if (!glfwInit())
+		return -1; // exit the system on the event of failure
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	window = glfwCreateWindow(900, 900, "mechanics_simulation", NULL, NULL);
+
+	if (!window) {
+		glfwTerminate();
+		return -1; // terminate glfw and exit system 
+	} else {
+		// glfwSetCursorPosCallback(window, mouse_callback);
+	}
+
+	glfwMakeContextCurrent(window);
+	std::string r;
+	std::cin >> r; 
+
+  	glfwTerminate();
+  	exit(EXIT_SUCCESS);
 }
